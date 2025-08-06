2832ac9be7a83c308f39b625ce22dd083fd9ef89
Yolande Dev <wwyolandeaudrey@gmail.com>
2025-08-06 10:35:39 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,234 @@
+import './App.css'
+import flearnaxylogo from './assets/flearnaxylogo.jpeg';
+function App(){
+  return(
+    <div className="blog">
+      <header className="header-blog">
+        <div className="flearnaxylogo">
+          <div className="logo">
+            <img src={flearnaxylogo} alt="" className='logo'/>
+          </div>
+          <div className="blog-img">
+            <img className="img-mini" src="http://beddingnewsnow.com/wp-content/uploads/2024/06/photos.png" alt="imge blog" />
+          </div>
+        </div>
+        <div className="link">
+          <a href="" className="student">For Students</a>
+          <a href="" className="tutor">For Tutors</a>
+        </div>
+        <div className="img-profile">
+          <img className="img-mini" src="http://beddingnewsnow.com/wp-content/uploads/2024/06/photos.png" alt="photo de profile" />
+        </div>
+      </header>
+
+      <main className="main-blog">
+        <div className="featured">
+          <h2>Featured Post</h2>
+          <div className="coll-featured">
+            <div className="container">
+              <h2 className='title'>How to Earn Fcoins as a student in flearnaxy</h2>
+              <p className='title'>in this blog you are going to learn what is fcoins. <br />
+              how to earn it and how u can use it to purchas items.</p>
+              <div className="mini-profiles">
+                <div className="img-mini">
+                  <img className="img-mini" src="http://beddingnewsnow.com/wp-content/uploads/2024/06/photos.png" alt="profile 1" />
+                </div>
+                <div className="by">
+                  <h4 className='title'>By Fonate Mimi</h4>
+                  <p className='title'> 2 minutes read</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="container">
+              <h2 className='title'>How to Earn Fcoins as a student in flearnaxy</h2>
+              <p className='title'>in this blog you are going to learn what is fcoins. <br />
+              how to earn it and how u can use it to purchas items.</p>
+              <div className="mini-profiles">
+                <div className="img-mini">
+                  <img className="img-mini" src="http://beddingnewsnow.com/wp-content/uploads/2024/06/photos.png" alt="profile 1" />
+                </div>
+                <div className="by">
+                  <h4 className='title'>By Fonate Mimi</h4>
+                  <p className='title' > 2 minutes read</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          <div className="coll-featured">
+            <div className="container">
+              <h2 className='title'>How to Earn Fcoins as a student in flearnaxy</h2>
+              <p className='title'>in this blog you are going to learn what is fcoins. <br />
+              how to earn it and how u can use it to purchas items.</p>
+              <div className="mini-profiles">
+                <div className="img-mini">
+                  <img className="img-mini" src="http://beddingnewsnow.com/wp-content/uploads/2024/06/photos.png" alt="profile 1" />
+                </div>
+                <div className="by">
+                  <h4 className='title'>By Fonate Mimi</h4>
+                  <p className='title'> 2 minutes read</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="container">
+              <h2 className='title'>How to Earn Fcoins as a student in flearnaxy</h2>
+              <p className='title'>in this blog you are going to learn what is fcoins. <br />
+              how to earn it and how u can use it to purchas items.</p>
+              <div className="mini-profiles">
+                <div className="img-mini">
+                  <img className="img-mini" src="http://beddingnewsnow.com/wp-content/uploads/2024/06/photos.png" alt="profile 1" />
+                </div>
+                <div className="by">
+                  <h4 className='title'>By Fonate Mimi</h4>
+                  <p className='title'> 2 minutes read</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="all-post">
+          <h2>All Posts</h2>
+          <div className="coll-featured">
+            <div className="container">
+              <h2 className='title'>How to Earn Fcoins as a student in flearnaxy</h2>
+              <p className='title'>in this blog you are going to learn what is fcoins. <br />
+              how to earn it and how u can use it to purchas items.</p>
+              <div className="mini-profiles">
+                <div className="img-mini">
+                  <img className="img-mini" src="http://beddingnewsnow.com/wp-content/uploads/2024/06/photos.png" alt="profile 1" />
+                </div>
+                <div className="by">
+                  <h4 className='title'>By Fonate Mimi</h4>
+                  <p className='title'> 2 minutes read</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="container">
+              <h2 className='title'>How to Earn Fcoins as a student in flearnaxy</h2>
+              <p className='title'>in this blog you are going to learn what is fcoins. <br />
+              how to earn it and how u can use it to purchas items.</p>
+              <div className="mini-profiles">
+                <div className="img-mini">
+                  <img className="img-mini" src="http://beddingnewsnow.com/wp-content/uploads/2024/06/photos.png" alt="profile 1" />
+                </div>
+                <div className="by">
+                  <h4 className='title'>By Fonate Mimi</h4>
+                  <p className='title'> 2 minutes read</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="container">
+              <h2 className='title'>How to Earn Fcoins as a student in flearnaxy</h2>
+              <p className='title'>in this blog you are going to learn what is fcoins. <br />
+              how to earn it and how u can use it to purchas items.</p>
+              <div className="mini-profiles">
+                <div className="img-mini">
+                  <img className="img-mini" src="http://beddingnewsnow.com/wp-content/uploads/2024/06/photos.png" alt="profile 1" />
+                </div>
+                <div className="by">
+                  <h4 className='title'>By Fonate Mimi</h4>
+                  <p className='title'> 2 minutes read</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="coll-featured">
+            <div className="container">
+              <h2 className='title'>How to Earn Fcoins as a student in flearnaxy</h2>
+              <p className='title'>in this blog you are going to learn what is fcoins. <br />
+              how to earn it and how u can use it to purchas items.</p>
+              <div className="mini-profiles">
+                <div className="img-mini">
+                  <img className="img-mini" src="http://beddingnewsnow.com/wp-content/uploads/2024/06/photos.png" alt="profile 1" />
+                </div>
+                <div className="by">
+                  <h4 className='title'>By Fonate Mimi</h4>
+                  <p className='title'> 2 minutes read</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="container">
+              <h2 className='title'>How to Earn Fcoins as a student in flearnaxy</h2>
+              <p className='title'>in this blog you are going to learn what is fcoins. <br />
+              how to earn it and how u can use it to purchas items.</p>
+              <div className="mini-profiles">
+                <div className="img-mini">
+                  <img className="img-mini" src="http://beddingnewsnow.com/wp-content/uploads/2024/06/photos.png" alt="profile 1" />
+                </div>
+                <div className="by">
+                  <h4 className='title'>By Fonate Mimi</h4>
+                  <p className='title'> 2 minutes read</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="container">
+              <h2 className='title'>How to Earn Fcoins as a student in flearnaxy</h2>
+              <p className='title'>in this blog you are going to learn what is fcoins. <br />
+              how to earn it and how u can use it to purchas items.</p>
+              <div className="mini-profiles">
+                <div className="img-mini">
+                  <img className="img-mini" src="http://beddingnewsnow.com/wp-content/uploads/2024/06/photos.png" alt="profile 1" />
+                </div>
+                <div className="by">
+                  <h4 className='title' >By Fonate Mimi</h4>
+                  <p className='title'> 2 minutes read</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <footer className="infos">
+        <div className="infos1">
+          <div className="flearnaxylogo">
+            <div className="logo">
+              <img src={flearnaxylogo} alt="" className='logo'/>
+            </div>
+            <div className="flearnaxy-image">
+              <p>Flearnaxy</p>
+            </div>    
+          </div>
+          <p>Empowering learners worldwide with quality education and <br />
+          innovative learning experiences</p>
+        </div>
+
+        <div className="column">
+          <div className="coll1">
+            <h4>For Students</h4>
+            <a href="#" className="links">Profile</a>
+            <a href="#" className="links">My learning</a>
+            <a href="#" className="links">Wishlist</a>
+            <a href="#" className="links">Earning F-coins</a>
+          </div>
+
+          <div className="coll1">
+            <h4>For Instuctors</h4>
+            <a href="#" className="links">Become aninstructor</a>
+            <a href="#" className="links">Teaching Ressources</a>
+            <a href="#" className="links">Course Creation</a>
+            <a href="#" className="links">Earning</a>
+          </div>
+
+          <div className="coll1">
+            <h4>Comoany</h4>
+            <a href="#" className="links">About Us</a>
+            <a href="#" className="links">Tems od Services</a>
+            <a href="#" className="links">Blog</a>
+            <a href="#" className="links">Privacy Pollcy</a>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
